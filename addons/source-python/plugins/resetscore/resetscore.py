2828251bafda4555b07c255a96ac8d4ffea7c265
Exiistenz <lilian78800@hotmail.fr>
2017-03-25 13:13:14 +0100

    Fix little mystake

--- addons/source-python/plugins/resetscore/resetscore.py
+++ addons/source-python/plugins/resetscore/resetscore.py
@@ -16,7 +16,7 @@ strings = LangStrings('resetscore')
 @SayCommand(['!rs', '/rs', '!resetscore', '/resetscore'])
 def _resetscrore_say_command(command, index, team_only=None):
     player = Player(index)
-    if player.kills != 0 and player.deaths != 0: 
+    if player.kills != 0 or player.deaths != 0: 
         player.kills = 0
         player.deaths = 0
         SayText2(strings['Resetscore']).send(player.index)
